525da116e26eebcb2ed5028d3bddd4a924331572
mike <michael.a.t.jay@gmail.com>
2021-08-10 22:06:26 -0400

    fist commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import About from "./components/About";
 
-function App() {
+// This site has 3 pages, all of which are rendered
+// dynamically in the browser (not server rendered).
+//
+// Although the page does not ever refresh, notice how
+// React Router keeps the URL up to date as you navigate
+// through the site. This preserves the browser history,
+// making sure things like the back button and bookmarks
+// work properly.
+
+export default function BasicExample() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Router>
+      <div id="app">
+        <div className="nav_container">
+        <div id="Nav">
+          <ul>
+            <li>
+            <Link id="home" to="/">About</Link>
+            </li>
+            <li>
+            <Link id="projects" to="/projects">Projects</Link>
+            </li>
+            <li>
+            <Link id="projects" to="/projects">Contact</Link>
+            </li>
+         
+          
+          </ul>
+          
 
-export default App;
+
+        </div>
+        </div>
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Switch>
+          <Route exact path="/">
+            <About />
+          </Route>
+          <Route path="/projects">
+          "dicks"
+          </Route>
+          <Route path="/dashboard">
+            "out"
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+      }
---
+++ src/components/About.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class About extends React.Component {
+    render(){
+       return (
+        <div className="about">
+            <div className="console">
+            <h1 className="terminal_name"><span className="host_name">Fullstack Application developer:</span></h1>
+            
+            &#9251;
+                <h1 className="terminal_name"><span className="host_name">Wb_Dv@Ap_Dv</span>: ~$  Mike Jay <span className="args">--help</span></h1>
+                 <h1><span className="args">Topic</span></h1>
+                 <h1><span className="args">&nbsp;&nbsp;&nbsp;&nbsp;Your future developer.</span></h1>
+                 <h1><span className="args">Description</span></h1>
+                 <h1><span className="args">&nbsp;&nbsp;&nbsp;&nbsp;Plans your project, implements the plan, and deploys the product.</span></h1>
+                <h1 className="terminal_name"><span className="host_name">Wb_Dv@Ap_Dv</span>: ~$ Plan Implement Deplo<span className="cursor">y</span></h1>
+            </div>
+        </div>
+       )
+    }
+}
+export default About;
